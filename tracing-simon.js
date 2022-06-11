c8eb12dd525e75f0aa58f5ded8b49b0cf2de507d
zx8086 <zx8086@mac.com@users.noreply.github.com>
2022-06-11 03:17:50 +0200

    remove code smells from tracing file

--- tracing-simon.js
+++ tracing-simon.js
@@ -17,16 +17,8 @@ const { RouterInstrumentation } = require('@opentelemetry/instrumentation-router
 const { SocketIoInstrumentation } = require('opentelemetry-instrumentation-socket.io');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 
-// const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-
-// const { PrometheusExporter } = require("@opentelemetry/exporter-prometheus");
-// const prometheusExporter = new PrometheusExporter({ startServer: true });
-
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
-  // metricExporter: prometheusExporter,
   instrumentations: 
     [
       getNodeAutoInstrumentations()
@@ -42,58 +34,14 @@ const provider = new NodeTracerProvider({
 
 const exporter = new OTLPTraceExporter();
 
-// --- Metrics Working Target Common
-
-// const metricExporter = new OTLPMetricExporter({});
-
-// const meterProvider = new MeterProvider({
-//   resource: new Resource({
-//         [SemanticResourceAttributes.SERVICE_NAME]: "simon-metrics-service",
-//         [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: "Production",
-//   }),
-// });
-
-// meterProvider.addMetricReader(new PeriodicExportingMetricReader({
-//   exporter: metricExporter,
-//   exportIntervalMillis: 60000,
-// }));
-
-// const meter = meterProvider.getMeter('example-exporter-collector');
-
-// const requestCounter = meter.createCounter('requests', {
-//   description: 'Example of a Counter',
-// });
-
-// const upDownCounter = meter.createUpDownCounter('test_up_down_counter', {
-//   description: 'Example of a UpDownCounter',
-// });
-
-// const histogram = meter.createHistogram('test_histogram', {
-//   description: 'Example of a Histogram',
-// });
-
-// const attributes = { pid: process.pid, environment: 'staging' };
-
-// setInterval(() => {
-//   requestCounter.add(1, attributes);
-//   upDownCounter.add(Math.random() > 0.5 ? 1 : -1, attributes);
-//   histogram.record(Math.random(), attributes);
-// }, 1000);
-
-// --
-
-// Configure span processor to send spans to the exporter
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 provider.register();
 
-// initialize the SDK and register with the OpenTelemetry API
-// this enables the API to record telemetry
 sdk.start()
   .then(() => console.log('Tracing initialized'))
   .catch((error) => console.log('Error initializing tracing', error));
 
-// gracefully shut down the SDK on process exit
 process.on('SIGTERM', () => {
   sdk.shutdown()
     .then(() => console.log('Tracing terminated'))
