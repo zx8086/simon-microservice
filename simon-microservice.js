d04c6d689835bb02dbe2f72c28b9f7bd39193c0d
zx8086 <zx8086@mac.com@users.noreply.github.com>
2022-06-15 03:41:50 +0200

    MongoDB instrumentation

--- simon-microservice.js
+++ simon-microservice.js
@@ -17,6 +17,7 @@ const logger = require('./logger')
 const httpLogger = require('./httpLogger')
 const cookieParser = require('cookie-parser')
 const { Kafka } = require('kafkajs')
+const { MongoClient } = require("mongodb");
 
 const csrf = require('csurf')
 
@@ -198,6 +199,30 @@ app.get('/simon', async (_req, res) => {
       logger.debug('This is the "/simon" route.')
     })
 })
+
+app.get("/todo", async (req, res) => {
+  const todos = await db.collection("todos").find({}).toArray();
+  res.send(todos);
+});
+
+app.get("/todo/:id", async (req, res) => {
+  const todo = await db
+    .collection("todos")
+    .findOne({ id: req.params.id });
+  res.send(todo);
+});
+
+
+const startServer = () => {
+  MongoClient.connect("mongodb://localhost:27017", (err, client) => {
+    db = client.db("todo");
+db.collection("todos").insertMany([
+     { id: "1", title: "Buy groceries" },
+     { id: "2", title: "Install Aspecto" },
+     { id: "3", title: "buy my own name domain" },
+   ]);
+ });
+  };
 
 console.log('Server initialized')
 
