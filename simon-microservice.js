b33692253fb90f2ce0e363fa983077d2cfe77e47
zx8086 <zx8086@mac.com@users.noreply.github.com>
2022-06-16 15:57:16 +0200

    Minor fixes

--- simon-microservice.js
+++ simon-microservice.js
@@ -6,7 +6,7 @@ dotenv.config()
 const aspectoAuth = process.env.ASPECTO_API_KEY
 
 const logger = require('./logger')
-const { setLogger } = instrument({local:true, logger: logger, aspectoAuth: aspectoAuth, serviceName: 'simon-microservice', env: 'Production', writeSystemLogs: true, exportBatchSize: 100, samplingRatio: 1.0, disableAspecto: false})
+const { setLogger } = instrument({ local: true, logger, aspectoAuth, serviceName: 'simon-microservice', env: 'Production', writeSystemLogs: true, exportBatchSize: 100, samplingRatio: 1.0, disableAspecto: false })
 
 // initialize your service ...
 setLogger(logger)
@@ -136,26 +136,25 @@ app.get('/health', function (_req, res) {
 })
 
 app.get('/consume', async function (_req, res) {
-
-const main = async () => {
-const consumer = kafkaInst.consumer({ groupId: 'quotes-group' })
-await consumer.connect()
-await consumer.subscribe({ topic: 'quotes', fromBeginning: false })
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log('Received message', {
-      topic,
-      partition,
-      key: message.key.toString(),
-      value: message.value.toString()
+  const main = async () => {
+    const consumer = kafkaInst.consumer({ groupId: 'quotes-group' })
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'quotes', fromBeginning: false })
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log('Received message', {
+          topic,
+          partition,
+          key: message.key.toString(),
+          value: message.value.toString()
+        })
+      }
     })
+    await consumer.disconnect()
+    res.end('Consumed all Quotes in the Kafka topic')
   }
-})
-await consumer.disconnect()
-res.end('Consumed all Quotes in the Kafka topic')
-}
 
-main().catch(async error => {
+  main().catch(async error => {
     console.error(error)
     try {
       logger.debug('Console Error....')
