2ed7a930bd6a45484a396e8a3e6308fbac0c69bf
zx8086 <zx8086@mac.com@users.noreply.github.com>
2022-06-19 04:11:07 +0200

    Small changes

--- kafka.js
+++ kafka.js
@@ -1,8 +1,7 @@
 const { Kafka } = require("kafkajs");
 
 const { KAFKA_USERNAME: username, KAFKA_PASSWORD: password } = process.env;
-const sasl =
-  username && password ? { username, password, mechanism: "plain" } : null;
+const sasl = username && password ? { username, password, mechanism: "plain" } : null;
 const ssl = !!sasl;
 
 // This creates a client instance that is configured to connect to the Kafka broker provided by
