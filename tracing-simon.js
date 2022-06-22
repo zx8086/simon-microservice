21563cd15e258f92cd614ae385472fadf3869659
zx8086 <zx8086@mac.com@users.noreply.github.com>
2022-06-22 22:44:08 +0200

    Minor fixes

--- tracing-simon.js
+++ tracing-simon.js
@@ -1,5 +1,6 @@
-/* tracing-app.js */
+/* tracing-simon.js */
 'use strict';
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -16,19 +17,18 @@ const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-htt
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { KafkaJsInstrumentation } = require('opentelemetry-instrumentation-kafkajs');
 const { RouterInstrumentation } = require('@opentelemetry/instrumentation-router');
 const { SocketIoInstrumentation } = require('opentelemetry-instrumentation-socket.io');
-const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+// const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+// // For troubleshooting, set the log level to DiagLogLevel.DEBUG
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  // traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
   instrumentations: 
     [
       getNodeAutoInstrumentations(),
@@ -48,8 +48,8 @@ const sdk = new opentelemetry.NodeSDK({
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME,
-    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.DEPLOYMENT_ENVIRONMENT
+      [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME,
+      [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.DEPLOYMENT_ENVIRONMENT
     }),
 });
 
@@ -60,29 +60,19 @@ const metricExporter = new OTLPMetricExporter({});
 
 const meterProvider = new MeterProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME,
-    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.DEPLOYMENT_ENVIRONMENT
+      [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME,
+      [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.DEPLOYMENT_ENVIRONMENT
   }),
 });
 
 // Configure span processor to send spans to the exporter
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-provider.addSpanProcessor(new SimpleSpanProcessor(new PeriodicExportingMetricReader(meterProvider, {    
-  interval: 1000,
-  exporter: metricExporter,
-  meterProvider: meterProvider,
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: process.env.SERVICE_NAME,
-    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.DEPLOYMENT_ENVIRONMENT
-  }),
-})));
-
-// provider.register();
+provider.register();
 
 sdk.start()
-.then(() => console.log('Tracing initialized'))
-  .catch((error) => console.log('Error initializing tracing', error));
+  .then(() => console.log('Tracing initialized'))
+    .catch((error) => console.log('Error initializing tracing', error));
   
   // gracefully shut down the SDK on process exit
   process.on('SIGTERM', () => {
@@ -91,4 +81,3 @@ sdk.start()
     .catch((error) => console.log('Error terminating tracing', error))
     .finally(() => process.exit(0));
     });
-
