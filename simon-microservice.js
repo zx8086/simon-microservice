2ad2fe7e9e757b4a0bed859cb86fa89d63625d16
Simon Owusu <1085655+zx8086@users.noreply.github.com>
2022-09-22 14:29:49 +0200

    Remove git pre-push hook

--- simon-microservice.js
+++ simon-microservice.js
@@ -1,19 +1,16 @@
-"use strict";
+"use strict"
 // const instrument = require('@aspecto/opentelemetry');
 // instrument({aspectoAuth: '1cbb856b-0558-4e75-876f-3aee212f65c7'});
 
-const dotenv = require("dotenv");
-dotenv.config();
+const dotenv = require("dotenv")
+dotenv.config()
 
 const handlers = require('./lib/handlers')
 const services = require('./lib/services')
-const logger = require("./lib/logger");
+const logger = require("./lib/logger")
 
-const quote = require('./lib/quote')
-
-
-const httpLogger = require("./httpLogger");
-const cookieParser = require("cookie-parser");
+const httpLogger = require("./httpLogger")
+const cookieParser = require("cookie-parser")
 // const csrf = require("csurf");
 // const csrfProtection = csrf({ cookie: false });
 const express = require("express")
@@ -24,7 +21,6 @@ const PORT = process.env.PORT || 8070
 
 app.use(httpLogger)
 app.use(cookieParser())
-
 app.get('/', handlers.home)
 app.get('/health', handlers.health)
 app.get('/external', handlers.external)
@@ -32,21 +28,20 @@ app.get('/twilio', services.twilio)
 app.get('/kafka', services.kafka)
 app.get('/couchbase', services.couchbase)
 app.get('/workplace', services.workplace)
-// app.get('/quote', quote.quote)
-
 app.use(handlers.notFound)
 app.use(handlers.serverError)
 
+
 app.listen(parseInt(PORT, 10), () => {
-  console.log(`Listening for requests on http://localhost:${PORT}`);
-  logger.info("Starting server.... Process initialized!");
-});
+  console.log(`Listening for requests on http://localhost:${PORT}`)
+  logger.info("Starting server.... Process initialized!")
+})
 
 process.on("SIGTERM", () => {
   app.close(() => {
-    logger.info("Stopping server.... Process terminated!");
-    console.log("Process terminated");
-  });
-});
+    logger.info("Stopping server.... Process terminated!")
+    console.log("Process terminated")
+  })
+})
 
 // "Beware of bugs in the above code; I have only proved it correct, not tried it." - Donald Knuth
