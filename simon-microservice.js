fb561759ffbfb5e05dc7ce6ba94393556f6b1c8e
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-05-29 03:40:01 +0200

    Adding Custom Headeer

--- simon-microservice.js
+++ simon-microservice.js
@@ -4,5 +4,4 @@ const { start } = require('./lib/server')
 
 start()
 
-// "Beware of bugs in the above code; I have only proved it correct, not tried it." - Donald Knuth
 // "The most important property of a program is whether it accomplishes the intention of its user." - C.A.R. Hoare
